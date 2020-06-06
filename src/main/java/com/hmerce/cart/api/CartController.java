c331662c09c323e2e81af66c1e675f9fd6de22ab
dragan <dragan.velkovski@codeit.mk>
2020-06-06 16:20:47 +0200

    return created cart

--- src/main/java/com/hmerce/cart/api/CartController.java
+++ src/main/java/com/hmerce/cart/api/CartController.java
@@ -1,6 +1,7 @@
 package com.hmerce.cart.api;
 
 
+import com.hmerce.cart.model.Cart;
 import com.hmerce.cart.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +16,7 @@ public class CartController {
     CartService cartService;
 
     @GetMapping
-    public String getCart(){
-        cartService.createCart();
-        return "Hello";
+    public Cart getCart(){
+        return cartService.createCart();
     }
 }
